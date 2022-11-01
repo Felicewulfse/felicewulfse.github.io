a124b50ec13eb61cd7b57cd72162950fd6184c36
Felicewulfse <96291397+Felicewulfse@users.noreply.github.com>
2022-11-01 16:28:28 +0100

    Add files via upload

--- code_functions.r
+++ code_functions.r
@@ -1,4 +1,18 @@
-mean <- function(x){
-mean <- mean(x)
-round(mean)
+# Install packages
+library(dplr)
+library(tidyverse)
+
+# Create function
+round_to_two <- function(x){
+round(x, digits =2)
 }
+
+# Create data
+data <- c(102.364,325.634,43.1,6345.2434543,72.22) %>% as.data.frame()
+
+# Test function
+round_to_two(data)
+
+
+
+
